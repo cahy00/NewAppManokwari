dd94185dc54a09f770f6f0dd530443a5430f6ed3
Sulistyo Cahyo G <cariercahyo@gmail.com>
2023-01-08 15:18:45 +0900

    #

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -4,12 +4,13 @@
     <meta charset="utf-8" />
     <meta content="width=device-width, initial-scale=1.0" name="viewport" />
 
-    <title>ZenBlog Bootstrap Template - Index</title>
+    <title>Kantor Regional XIV Manokwari</title>
     <meta content="" name="description" />
     <meta content="" name="keywords" />
 
     <!-- Favicons -->
-    <link href="assets/img/favicon.png" rel="icon" />
+    {{-- <link href="assets/img/favicon.png" rel="icon" /> --}}
+		<link href="{{asset('bkn/icon-garuda.ico')}}" rel="icon">
     <link href="assets/img/apple-touch-icon.png" rel="apple-touch-icon" />
 
     <!-- Google Fonts -->
