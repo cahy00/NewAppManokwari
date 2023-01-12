3bb8784cc8f5c2c1feb3be350accb5f4f5a107b9
Sulistyo Cahyo G <cariercahyo@gmail.com>
2023-01-12 09:34:27 +0900

    #

--- resources/views/user-layouts/footer.blade.php
+++ resources/views/user-layouts/footer.blade.php
@@ -18,32 +18,27 @@
 				<ul class="footer-links list-unstyled">
 					<li>
 						<a href="index.html"
-							><i class="bi bi-chevron-right"></i> Home</a
+							><i class="bi bi-chevron-right"></i> Beranda</a
 						>
 					</li>
 					<li>
 						<a href="index.html"
-							><i class="bi bi-chevron-right"></i> Blog</a
+							><i class="bi bi-chevron-right"></i> Profil</a
 						>
 					</li>
 					<li>
 						<a href="category.html"
-							><i class="bi bi-chevron-right"></i> Categories</a
+							><i class="bi bi-chevron-right"></i> Kategori</a
 						>
 					</li>
 					<li>
 						<a href="single-post.html"
-							><i class="bi bi-chevron-right"></i> Single Post</a
+							><i class="bi bi-chevron-right"></i> Pelayanan</a
 						>
 					</li>
 					<li>
 						<a href="about.html"
-							><i class="bi bi-chevron-right"></i> About us</a
-						>
-					</li>
-					<li>
-						<a href="contact.html"
-							><i class="bi bi-chevron-right"></i> Contact</a
+							><i class="bi bi-chevron-right"></i> Kontak</a
 						>
 					</li>
 				</ul>
@@ -53,137 +48,43 @@
 				<ul class="footer-links list-unstyled">
 					<li>
 						<a href="category.html"
-							><i class="bi bi-chevron-right"></i> Business</a
+							><i class="bi bi-chevron-right"></i> Berita Kepegawaian</a
 						>
 					</li>
 					<li>
 						<a href="category.html"
-							><i class="bi bi-chevron-right"></i> Culture</a
-						>
-					</li>
-					<li>
-						<a href="category.html"
-							><i class="bi bi-chevron-right"></i> Sport</a
-						>
-					</li>
-					<li>
-						<a href="category.html"
-							><i class="bi bi-chevron-right"></i> Food</a
-						>
-					</li>
-					<li>
-						<a href="category.html"
-							><i class="bi bi-chevron-right"></i> Politics</a
-						>
-					</li>
-					<li>
-						<a href="category.html"
-							><i class="bi bi-chevron-right"></i> Celebrity</a
-						>
-					</li>
-					<li>
-						<a href="category.html"
-							><i class="bi bi-chevron-right"></i> Startups</a
-						>
-					</li>
-					<li>
-						<a href="category.html"
-							><i class="bi bi-chevron-right"></i> Travel</a
+							><i class="bi bi-chevron-right"></i> Artikel Kepegawaian</a
 						>
 					</li>
 				</ul>
 			</div>
 
 			<div class="col-lg-4">
-				<h3 class="footer-heading">Recent Posts</h3>
-
-				<ul class="footer-links footer-blog-entry list-unstyled">
+				<h3 class="footer-heading">Sosial Media</h3>
+				<ul class="footer-links list-unstyled">
 					<li>
-						<a href="single-post.html" class="d-flex align-items-center">
-							<img
-								src="assets/img/post-sq-1.jpg"
-								alt=""
-								class="img-fluid me-3"
-							/>
-							<div>
-								<div class="post-meta d-block">
-									<span class="date">Culture</span>
-									<span class="mx-1">&bullet;</span>
-									<span>Jul 5th '22</span>
-								</div>
-								<span>5 Great Startup Tips for Female Founders</span>
-							</div>
-						</a>
+						<a href="category.html"
+							><i class="bi bi-facebook"></i> Facebook</a
+						>
 					</li>
-
 					<li>
-						<a href="single-post.html" class="d-flex align-items-center">
-							<img
-								src="assets/img/post-sq-2.jpg"
-								alt=""
-								class="img-fluid me-3"
-							/>
-							<div>
-								<div class="post-meta d-block">
-									<span class="date">Culture</span>
-									<span class="mx-1">&bullet;</span>
-									<span>Jul 5th '22</span>
-								</div>
-								<span
-									>What is the son of Football Coach John Gruden, Deuce
-									Gruden doing Now?</span
-								>
-							</div>
-						</a>
+						<a href="#" class="twitter"><i class="bi bi-twitter"></i> Twitter</a>
 					</li>
-
 					<li>
-						<a href="single-post.html" class="d-flex align-items-center">
-							<img
-								src="assets/img/post-sq-3.jpg"
-								alt=""
-								class="img-fluid me-3"
-							/>
-							<div>
-								<div class="post-meta d-block">
-									<span class="date">Culture</span>
-									<span class="mx-1">&bullet;</span>
-									<span>Jul 5th '22</span>
-								</div>
-								<span
-									>Life Insurance And Pregnancy: A Working Mom’s
-									Guide</span
-								>
-							</div>
-						</a>
+						<a href="category.html"
+							><i class="bi bi-chevron-right"></i> Instagram</a
+						>
 					</li>
-
 					<li>
-						<a href="single-post.html" class="d-flex align-items-center">
-							<img
-								src="assets/img/post-sq-4.jpg"
-								alt=""
-								class="img-fluid me-3"
-							/>
-							<div>
-								<div class="post-meta d-block">
-									<span class="date">Culture</span>
-									<span class="mx-1">&bullet;</span>
-									<span>Jul 5th '22</span>
-								</div>
-								<span
-									>How to Avoid Distraction and Stay Focused During Video
-									Calls?</span
-								>
-							</div>
-						</a>
+						<a href="category.html"
+							><i class="bi bi-chevron-right"></i> Tiktok</a
+						>
 					</li>
 				</ul>
 			</div>
 		</div>
 	</div>
 </div>
-
 <div class="footer-legal">
 	<div class="container">
 		<div class="row justify-content-between">
@@ -205,8 +106,8 @@
 
 			<div class="col-md-6">
 				<div class="social-links mb-3 mb-lg-0 text-center text-md-end">
-					<a href="#" class="twitter"><i class="bi bi-twitter"></i></a>
-					<a href="#" class="facebook"><i class="bi bi-facebook"></i></a>
+					
+					<a href="#" class="facebook"></a>
 					<a href="#" class="instagram"
 						><i class="bi bi-instagram"></i
 					></a>
